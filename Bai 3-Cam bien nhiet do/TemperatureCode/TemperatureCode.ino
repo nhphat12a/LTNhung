4dc9e06ccc22709773113e48fca8b9504c35e4a7
nhphat12a <ngpht53@gmail.com>
2021-10-08 21:34:21 +0700

    Bài 3: cảm biến nhiệt độ (đã sửa lỗi)

--- Bai 3-Cam bien nhiet do/TemperatureCode/TemperatureCode.ino
+++ Bai 3-Cam bien nhiet do/TemperatureCode/TemperatureCode.ino
@@ -1,13 +1,13 @@
 void setup()
 {
   pinMode(10,OUTPUT);
-  Serial.begin(96000);
+  Serial.begin(9600);
 }
 void loop()
 {
   int GiaTri = analogRead(A0);
   int nhietdo=map(GiaTri,20,358,-40,125);
-  //Serial.print(nhietdo);
+  Serial.print(nhietdo);
   if (nhietdo>37) digitalWrite(10,HIGH);
   else digitalWrite(10,LOW);
   delay(1000);
